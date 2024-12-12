bcbf35d675a63268fdc0129864e627510e517a13
drowninqq <vadimsomnia@gmail.com>
2024-12-12 13:25:25 +0300

    added english

--- program.cpp
+++ program.cpp
@@ -8,17 +8,24 @@ void russian() {
     cout << "Приветствие на 'русском' языке: Привет, мир!" << endl;
 }
 
+void english() {
+    cout << "Приветствие на 'английском' языке: Hello, World!" << endl;
+}
+
 int main() {
     setlocale(LC_ALL, "");
 
     string language;
-    cout << "Поддерживаемые языки: russian" << endl;
+    cout << "Поддерживаемые языки: russian, english" << endl;
     cout << "Введите язык: ";
     cin >> language;
 
     if (language == "russian") {
         russian();
-    }
+    } 
+    else if (language == "english") {
+        english();
+    } 
     else {
         cout << "Язык не поддерживается." << endl;
     }
